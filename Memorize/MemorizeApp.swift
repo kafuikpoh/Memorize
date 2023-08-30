9d306a22db433925ba0f961cf6a96476f6e65b61
Kafui Kpoh <qaphuikpoh@gmail.com>
2023-08-30 06:21:36 +0000

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Kafui Kpoh on 30/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
