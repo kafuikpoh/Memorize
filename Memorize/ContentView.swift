4f6ab39a456baa8d187123212a1b78811ccc131b
Kafui Kpoh <qaphuikpoh@gmail.com>
2023-09-06 12:20:18 +0000

    Add themes to cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,11 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let emojis: [String] = ["👻", "🎃", "🕷️", "👹", "💀", "🕸️"]
-    @State var cardCount: Int = 4
+    @State var emojis: Array<String> = []
+    
+    
+    @State var cardCount: Int = 12
+    @State var cardColourTheme: Color = .white
+    
     
     var body: some View {
         VStack {
+            Text("Memorize!").font(.largeTitle)
             
             ScrollView {
                 cards
@@ -21,53 +26,111 @@ struct ContentView: View {
             
             Spacer()
             
-            cardCountAdjusters
+            HStack(alignment: .bottom) {
+                Button(action: {
+                    emojis = theme(name: "vehicles")
+                    cardColourTheme = .orange
+                }, label: {
+                    VStack {
+                        Image(systemName: "car").imageScale(.large)
+                        Text("Vehicles")
+                    }
+                })
+                
+                Spacer()
+                
+                Button(action: {
+                    emojis = theme(name: "halloween")
+                    cardColourTheme = .red
+                }, label: {
+                    VStack {
+                        Image(systemName: "party.popper").imageScale(.large)
+                        Text("Halloween")
+                    }
+                })
+                
+                Spacer()
+                
+                Button(action: {
+                    emojis = theme(name: "party")
+                    cardColourTheme = .yellow
+                }, label: {
+                    VStack {
+                        Image(systemName: "balloon").imageScale(.large)
+                        Text("Party")
+                    }
+                })
+            }.padding()
         }
         .padding()
     }
     
-    var cardCountAdjusters: some View {
-        HStack {
-            cardRemover
-            .imageScale(.large)
-            .font(.largeTitle)
+//    var cardCountAdjusters: some View {
+//        HStack {
+//            cardRemover
+//            .imageScale(.large)
+//            .font(.largeTitle)
+//
+//            Spacer()
+//
+//            cardAdder
+//            .imageScale(.large)
+//            .font(.largeTitle)
+//        }
+//    }
+//
+//    func cardCountAdjusters (by offset: Int, symbol: String) -> some View {
+//        Button(action: {
+//            cardCount += offset
+//
+//        }, label: {
+//            Image(systemName: symbol)
+//        })
+//        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+//    }
+    func theme(name: String) -> [String] {
+        var emojiList: [String] = []
+        
+        switch name {
+        case "vehicles":
             
-            Spacer()
+            emojiList = ["🚗","🚕","🚌","🏎️","🚚","🚔","🚗","🚕","🚌","🏎️","🚚","🚔"]
             
-            cardAdder
-            .imageScale(.large)
-            .font(.largeTitle)
+        case "halloween":
+            
+            emojiList = ["👻", "🎃", "🕷️", "👹", "💀", "🕸️","👻", "🎃", "🕷️", "👹", "💀", "🕸️"]
+            
+        case "party":
+            
+            emojiList = ["🍤","🍕","🍢","🎂","🥳","🪅", "🍤","🍕","🍢","🎂","🥳","🪅"]
+            
+        default:
+            emojiList = []
         }
-    }
-    
-    func cardCountAdjusters (by offset: Int, symbol: String) -> some View {
-        Button(action: {
-            cardCount += offset
-            
-        }, label: {
-            Image(systemName: symbol)
-        })
-        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+        
+        return emojiList.shuffled()
     }
     
     var cards: some View {
-        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 80))]) {
+            
             ForEach(0..<cardCount, id: \.self){ index in
-                CardView(content: emojis[index])
+                
+                CardView(content: emojis.count > 0 ? emojis[index] : "")
                     .aspectRatio(2/3, contentMode: .fit)
             }
             
         }
-        .foregroundColor(.orange)
+        .foregroundColor(cardColourTheme)
     }
-    
-    var cardRemover: some View {
-        cardCountAdjusters(by: -1, symbol: "rectangle.stack.badge.minus.fill")
-    }
-    
-    var cardAdder: some View {
-        cardCountAdjusters(by: +1, symbol: "rectangle.stack.badge.plus.fill")
-    }
+//
+//    var cardRemover: some View {
+//        cardCountAdjusters(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+//    }
+//
+//    var cardAdder: some View {
+//        cardCountAdjusters(by: +1, symbol: "rectangle.stack.badge.plus.fill")
+//    }
 }
 
 struct CardView: View {
